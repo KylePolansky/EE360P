dccc66ac3bb1ff0cef66cf0d107ff46339cd2f4a
Javier Cortes <javiercortescen@gmail.com>
2017-02-13 14:38:18 -0600

    no message

--- HW2/q2/FairReadWriteLock.java
+++ HW2/q2/FairReadWriteLock.java
@@ -1,16 +1,23 @@
+
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
 //EIDS=KPP446,JC82563
 public class FairReadWriteLock {
-	
-                        
-	public synchronized void beginRead() {
-	}
-	
-	public synchronized void endRead() {
-	}
-	
-	public synchronized void beginWrite() {
-	}
-	public synchronized void endWrite() {
-	}
+    ReentrantReadWriteLock lock = new ReentrantReadWriteLock(true);
+    
+    public synchronized void beginRead() throws InterruptedException {
+        lock.readLock().lock();
+    }
+
+    public synchronized void endRead() {
+        lock.readLock().unlock();
+    }
+
+    public synchronized void beginWrite() throws InterruptedException{
+       lock.writeLock().lock();
+    }
+    public synchronized void endWrite() {
+        lock.writeLock().unlock();
+    }
 }
 	
