49f25def423f0d9902319df2b3c2661e7e73c235
Javier Cortes <javiercortescen@gmail.com>
2017-04-09 00:34:47 -0500

    Fixed MPI's reading from file. Cleaned code a bit.

--- HW5/MPI/product.c
+++ HW5/MPI/product.c
@@ -1,6 +1,10 @@
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+int readVectorFromFile(int ** v, FILE *file);
+int * computeProduct(int **mat, int *v, int N, int M);
 
 int main(int argc, char** argv) {
     int i,j,k;
@@ -19,91 +23,73 @@ int main(int argc, char** argv) {
 
     int *array_sizes = NULL;
     int chunk_size;
-    int *read_mat=NULL;
-    int* mat_sizes=NULL;
-    int N;
+    int *mat_send=NULL;
+    int* v=NULL;
+    int* lines=NULL;
+    int N,M;
+
+    //READ MATRIX AND VECTOR
     if(world_rank == 0)
     {
-        //READ MATRIX INTO A 1D VECTOR
-        FILE *file;
-        file=fopen("matrix.txt", "r");
-        fscanf(file, "%d", &N);
+        FILE *Mfile;
+        Mfile=fopen("matrix.txt", "r");
+        FILE *Vfile;
+        Vfile=fopen("vector.txt", "r");
 
-        read_mat=malloc(N*N*sizeof(int*));
+        fscanf(Mfile, "%d", &N);
 
-        for(i = 0; i < N*N; i++)
-        {
-            if (!fscanf(file, "%d", &read_mat[i]))
-                 break;
-        }
-        fclose(file);
-    }
-
-    //Send N to all processes
-    MPI_Bcast( &N, 1, MPI_INT, 0, MPI_COMM_WORLD );
-
-    int* v=malloc(N*sizeof(int*));
-
-    if(world_rank == 0)
-    {
-        //READ VECTOR
-        FILE *file;
-        file=fopen("vector.txt", "r");
-
-        for(i = 0; i < N; i++)
-        {
-            if (!fscanf(file, "%d", &v[i]))
-                 break;
-        }
-        fclose(file);
+        readVectorFromFile(&mat_send, Mfile);
+        M=readVectorFromFile(&v,Vfile);
 
         int general_chunk_size = N / world_size;
-        mat_sizes = (int *)malloc(world_size*sizeof(int *));
+        lines = (int *)malloc(world_size*sizeof(int *));
         for (int i=0; i<world_size-1; i++)
         {
-            mat_sizes[i]=general_chunk_size;
+            lines[i]=general_chunk_size;
         }
-        mat_sizes[world_size-1]=N-(world_size-1)*general_chunk_size;
+        lines[world_size-1]=N-(world_size-1)*general_chunk_size;
     }
+    
 
+    //Send M to all processes
+    MPI_Bcast( &M, 1, MPI_INT, 0, MPI_COMM_WORLD );
+    if(world_rank != 0)
+        v=malloc(M*sizeof(int*));
+    
     //Send v to all processes
-    MPI_Bcast( v, N, MPI_INT, 0, MPI_COMM_WORLD );
+    MPI_Bcast( v, M, MPI_INT, 0, MPI_COMM_WORLD );
 
     //Send chunk size to all processes
-    MPI_Scatter( mat_sizes, 1, MPI_INT, &chunk_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-    //printf("Process %d: Chunk size: %d\n", world_rank, chunk_size);
+    MPI_Scatter( lines, 1, MPI_INT, &chunk_size, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
     //Send corresponding lines to all processes
-    int *mat_displs = NULL;
+    int *mat_displacements = NULL;
     int *snd_counts=NULL;
     if(world_rank == 0)
     {
-        mat_displs = (int *)malloc(world_size*sizeof(int));
-
-        mat_displs[0]=0;
-
-        for(i=1; i<world_size; i++)
-        {
-            mat_displs[i]=mat_displs[i-1]+mat_sizes[i-1]*N;
-        }
-
         snd_counts=malloc(world_size*sizeof(int*));
         for(int i=0; i<world_size; i++)
-            snd_counts[i]=N*mat_sizes[i];
+            snd_counts[i]=M*lines[i];
+
+        mat_displacements = (int *)malloc(world_size*sizeof(int));
+
+        mat_displacements[0]=0;
+        for(i=1; i<world_size; i++)
+            mat_displacements[i]=mat_displacements[i-1]+snd_counts[i-1];
     }
 
-    int *mat_1d=malloc(N*chunk_size*sizeof(int*));
-    MPI_Scatterv( read_mat, snd_counts, mat_displs, MPI_INT, mat_1d, N*chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+    int *mat_1d=malloc(M*chunk_size*sizeof(int*));
+    MPI_Scatterv( mat_send, snd_counts, mat_displacements, MPI_INT, mat_1d, M*chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
 
+    //Rearrange into matrix form
     int **mat=malloc(chunk_size*sizeof(int*));
     for(i=0;i<chunk_size;i++)
-        mat[i]=malloc(N*sizeof(int));
+        mat[i]=malloc(M*sizeof(int));
 
     k=0;
     for(int i=0; i<chunk_size; i++)
     {
-        for(int j=0; j<N; j++)
+        for(int j=0; j<M; j++)
         {
             mat[i][j]=mat_1d[k];
             k++;
@@ -112,14 +98,7 @@ int main(int argc, char** argv) {
 
     //Compute product
     k=0;
-    int *res = (int *)malloc(chunk_size*sizeof(int));
-    for(i=0; i<chunk_size; i++)
-    {
-        res[k]=0;
-        for(j=0; j<N; j++)
-          res[k]+=mat[i][j]*v[j];
-        k++;
-    }
+    int *res = computeProduct(mat,v,chunk_size,M);
 
     //Collect results
     int *final_results=NULL;
@@ -127,20 +106,20 @@ int main(int argc, char** argv) {
     if(world_rank == 0)
     {
         final_results=(int (*))malloc(sizeof(int)*N);
-        final_displs = (int *)malloc(N*sizeof(int));
+        final_displs = (int *)malloc(world_size*sizeof(int));
 
         final_displs[0]=0;
 
-        for(i=1; i<N; i++)
+        for(i=1; i<world_size; i++)
         {
-            final_displs[i]=final_displs[i-1]+mat_sizes[i-1];
+            final_displs[i]=final_displs[i-1]+lines[i-1];
         }
 
     }
 
-    MPI_Gatherv(res, chunk_size, MPI_INT, final_results, mat_sizes, final_displs, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gatherv(res, chunk_size, MPI_INT, final_results, lines, final_displs, MPI_INT, 0, MPI_COMM_WORLD);
     
-    //Pprint results to file
+    //Print results to file
     if (world_rank==0)
     {
         FILE *file=fopen("result.txt", "wb");
@@ -154,3 +133,39 @@ int main(int argc, char** argv) {
     // Finalize the MPI environment. No more MPI calls can be made after this
     MPI_Finalize();
 }
+
+int readVectorFromFile(int ** v, FILE *file)
+{
+    int i=0;
+
+    int *temp=malloc(1000*sizeof(int));
+
+    while(fscanf(file, "%d", &temp[i]) != EOF)
+    {
+        i++;
+    }    
+    
+    fclose(file);
+
+    (*v)=malloc(i*sizeof(int*));
+    memcpy((*v), temp, i * sizeof(int));
+    free(temp);
+
+    return i;
+}
+
+int * computeProduct(int **mat, int *v, int N, int M)
+{
+    int i,j;
+    int k=0;
+    int *res = (int *)malloc(N*sizeof(int));
+    for(i=0; i<N; i++)
+    {
+        res[k]=0;
+        for(j=0; j<M; j++)
+            res[k]+=mat[i][j]*v[j];
+        k++;
+    }
+    return res;
+}
+
